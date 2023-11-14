24110a0d42837811b594e97980869ddb433fe362
hdf3093 <hdf3093@nyu.edu>
2023-11-14 11:06:38 -0500

    markdown test 2

--- markdown.py
+++ markdown.py
@@ -1,18 +1,4 @@
 import csv
-
-input_csv_path = "data/listings.csv"
-
-output_md_path = "README.md"
-
-with open(input_csv_path, "rU") as csvfile:
-    reader = csv.reader(csvfile)
-
-    rows = [next(reader) for _ in range(3)]
-
-with open(output_md_path, 'a') as mdfile:
-
-    mdfile.write("| " + " | ".join(rows[0]) + " |\n")
-    mdfile.write("|" + ":---:|" * len(rows[0]) + "\n")
-
-    for row in rows[1:]:
-        mdfile.write("| " + " | ".join(row) + " |\n")
+reader = csv.reader(open("data/listings.csv", "r"), delimiter=',')
+writer = csv.writer(open("data/markdown.csv", 'w'), delimiter='|')
+writer.writerows(reader)
